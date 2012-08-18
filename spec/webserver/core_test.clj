6faf23d5132aa60d0266e20dac31e9b2d9013c90
Jeremy Neander <subjectego@gmail.com>
2012-08-17 19:33:25 -0500

    Add method #get-host
    This method should receive a vector of strings, each being one line from
    the HTTP request header, and return the string value associated with the
    "Host" field in the HTTP request header. If no value is found, nil is
    returned.

--- spec/webserver/core_test.clj
+++ spec/webserver/core_test.clj
@@ -7,6 +7,16 @@
         '(java.io OutputStream PrintStream StringReader))
 
 (def short-header (str "one\ntwo\n\nthree\n"))
+(def full-header (str
+  "GET / HTTP/1.1\n"
+  "Host: localhost:8080\n"
+  "Connection: keep-alive\n"
+  "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) "
+  "AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\n"
+  "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n"
+  "Accept-Encoding: gzip,deflate,sdch\n"
+  "Accept-Language: en-US,en;q=0.8\n"
+  "Accept-Charset: ISO-8859-1,utf-8;q=0.7\n\n"))
 
 (def client-output-mock (proxy [java.io.ByteArrayOutputStream] []))
 (def client-input-mock (proxy [java.io.StringBufferInputStream] ["test-string"]))
@@ -69,3 +79,11 @@
               (should= ["one" "two"] header-lines))
           (it "stops at empty line"
               (should-not (some #{"three"} header-lines))))
+
+(describe "#get-host"
+          (before (def header-lines (get-header-lines (mock-client-reader full-header)))
+                  (def failing-lines (get-header-lines (mock-client-reader short-header))))
+          (it "returns 'Host' value when found"
+              (should= "localhost:8080" (get-host header-lines)))
+          (it "returns nil otherwise"
+              (should= nil (get-host failing-lines))))
--- src/webserver/core.clj
+++ src/webserver/core.clj
@@ -32,3 +32,11 @@
     (if (< 0 (.length line)) 
       (recur line (.readLine client-reader) (conj lines line))
       lines)))
+
+(defn get-host [header-lines]
+  (loop [lines header-lines]
+    (if-not (empty? lines)
+      (let [host-value (re-find #"^Host: (.*)" (first lines))]
+        (if (nil? host-value)
+          (recur (rest lines))
+          (last host-value))))))
