8186692c0c5ad7fc5d273e427ef49d7e110f3613
Jeremy Neander <subjectego@gmail.com>
2012-08-18 22:22:42 -0500

    Make method #-main open and manage the server
    Method #-main should open a server socket on a given port (8080 by
    default), and repeatedly call method #listen-and-respond until the
    server socket is closed or the server is stopped.

--- src/webserver/core.clj
+++ src/webserver/core.clj
@@ -1,6 +1,12 @@
-(ns webserver.core)
+(ns webserver.core
+  (:use (webserver socket-connection
+                   request-handler)))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+<<<<<<< Updated upstream
+(defn -main [& args]
+=======
+(defn -main [& port]
+>>>>>>> Stashed changes
+  (let [server-socket (open-server-socket 8080)]
+    (while (not (.isClosed server-socket))
+      (listen-and-respond server-socket respond-to-request))))
