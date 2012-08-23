4277adb6a23c3bbdc94bde556e0cc14466c5f42f
Jeremy Neander <subjectego@gmail.com>
2012-08-23 14:41:42 -0500

    Make #main receive a port as an argument
    Method #main should be able to receive a port number to be used for the
    server. If no port argument is received, the default of 8080 should be
    used.

--- src/webserver/core.clj
+++ src/webserver/core.clj
@@ -3,6 +3,7 @@
                    request-handler)))
 
 (defn -main [& args]
-  (let [server-socket (open-server-socket 8080)]
+  (let [port (if (empty? args) 8080 (read-string (first args)))
+        server-socket (open-server-socket port)]
     (while (not (.isClosed server-socket))
       (listen-and-respond server-socket print-response))))
