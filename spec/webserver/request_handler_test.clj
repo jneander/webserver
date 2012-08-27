b853ec5c3d6b97e2304a9e8ebeca2f4da8166852
Jeremy Neander <subjectego@gmail.com>
2012-08-27 13:15:19 -0500

    Refactor
    New methods have been implemented which condense and replace existing
    implementations.

--- spec/webserver/request_handler_test.clj
+++ spec/webserver/request_handler_test.clj
@@ -1,128 +1,53 @@
 (ns webserver.request-handler-test
-  (:use speclj.core
-        webserver.request-handler)
-  (:import (java.io BufferedReader StringReader PrintStream OutputStream)))
+  (:require [speclj.core :refer [describe it should= should]]
+            [webserver.request-handler :refer [print-response]])
+  (:import [java.io BufferedReader StringReader PrintStream OutputStream]))
 
-(def short-header (str "one\ntwo\n\nthree\n"))
-(def full-header (str
-  "GET /foo/bar HTTP/1.1\n"
-  "Host: localhost:8080\n"
-  "Connection: keep-alive\n"
-  "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) "
-  "AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\n"
-  "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n"
-  "Accept-Encoding: gzip,deflate,sdch\n"
-  "Accept-Language: en-US,en;q=0.8\n"
-  "Accept-Charset: ISO-8859-1,utf-8;q=0.7\n\n"))
+(defn- sample-request-header [path]
+  (str "GET /spec/public_html/" path " HTTP/1.1\r\n"
+       "Host: localhost:8080\r\n\r\n"))
+
+(defn- sample-request-output []
+  (str "HTTP/1.1 200 OK\r\n"
+       "Host: localhost:8080\r\n"
+       "Content-Type: text/html\r\n"
+       "Content-Length: 7\r\n"
+       "\r\n"
+       "foobar\n"))
+
+(defn mock-client-reader [string]
+  (BufferedReader. (StringReader. string)))
 
 (defn mock-client-writer [tracker]
   (let [output-stream (proxy [OutputStream] [])]
     (proxy [PrintStream] [output-stream]
       (println [arg] (swap! tracker conj arg)))))
-(defn mock-client-reader [string]
-  (BufferedReader. (StringReader. string)))
-(defn generate-request []
-  (def header-lines (get-header-lines (mock-client-reader full-header)))
-  (def request-fields (map-request-fields header-lines)))
-(defn generate-response []
-  (def response-body (get-response-body request-fields))
-  (def response-map (get-response-map header-lines response-body))
-  (def response-header (get-response-header response-map)))
+
+(defn- tracker []
+  (atom []))
+
+(defn- track-request [request]
+  (let [output-tracker (tracker)
+        client-reader (mock-client-reader request)
+        client-writer (mock-client-writer output-tracker)]
+    (print-response client-reader client-writer)
+    output-tracker))
+
 (defn string-contains? [pattern source]
   (= pattern (re-find (re-pattern pattern) source)))
 
-(describe "#read-request-header"
-  (let [header (read-request-header (mock-client-reader short-header))]
-    (it "reads up to empty line"
-      (should= "one\r\ntwo\r\n" header))))
-
-(describe "#parse-get-request"
-          (it "matches to a slash"
-            (should= "/" (parse-get-request "GET / HTTP/1.1")))
-          (it "does not match"
-            (should= nil (parse-get-request "FOO GET / HTTP")))
-          (it "matches to longer request"
-            (should= "/foo/bar/" (parse-get-request "GET /foo/bar/ HTTP"))))
-
-(describe "#get-header-lines"
-          (before (def header-lines (get-header-lines (mock-client-reader short-header))))
-          (it "returns a vector of strings"
-              (should= java.lang.String (class (first header-lines))))
-          (it "reads up to empty line"
-              (should= ["one" "two"] header-lines))
-          (it "stops at empty line"
-              (should-not (some #{"three"} header-lines))))
-
-(describe "#get-host"
-          (before (def header-lines (get-header-lines (mock-client-reader full-header)))
-                  (def failing-lines (get-header-lines (mock-client-reader short-header))))
-          (it "returns 'Host' value when found"
-              (should= "localhost:8080" (get-host header-lines)))
-          (it "returns nil otherwise"
-              (should= nil (get-host failing-lines))))
-
-(describe "#respond-to-request"
-          (before (def tracker (atom []))
-                  (def client-reader (mock-client-reader full-header))
-                  (def client-writer (mock-client-writer tracker))
-                  (respond-to-request client-reader client-writer))
-          (it "prints the host name"
-              (should= "localhost" (some #{"localhost"} @tracker)))
-          (it "prints the port number"
-              (should= "8080" (some #{"8080"} @tracker)))
-          (it "prints the request path"
-              (should= "/foo/bar" (some #{"/foo/bar"} @tracker))))
-
-(describe "#map-request-fields"
-          (before (def request-fields (map-request-fields header-lines)))
-          (it "maps the host"
-              (should= "localhost:8080" (:host request-fields)))
-          (it "maps the path"
-              (should= "/foo/bar" (:path request-fields))))
-
-(describe "#get-response-body"
-          (before (def response-body (get-response-body 
-                                       (map-request-fields header-lines))))
-          (it "includes the host name"
-              (should (string-contains? "<p>localhost</p>" response-body)))
-          (it "includes the port"
-              (should (string-contains? "<p>8080</p>" response-body)))
-          (it "includes the path"
-              (should (string-contains? "<p>/foo/bar</p>" response-body))))
-
-(describe "#get-response-map"
-          (before-all (generate-request)
-                  (generate-response))
-          (it "maps the status code to 200"
-              (should= 200 (:status response-map)))
-          (it "maps the host to 'localhost:8080'"
-              (should= "localhost:8080" (:host response-map)))
-          (it "maps the content-type to 'text/html'"
-              (should= "text/html" (:content-type response-map)))
-          (it "maps the content-length to the length of the body"
-              (should= (.length response-body) (:content-length response-map))))
-
-(describe "#get-response-header"
-          (before-all (generate-request)
-                      (generate-response))
-          (it "includes the status code"
-              (should (string-contains? "HTTP/1.1 200 OK" response-header)))
-          (it "includes the host"
-              (should (string-contains? "Host: localhost:8080" response-header)))
-          (it "includes the content length"
-              (should (string-contains? "Content-Type: text/html" response-header)))
-          (it "includes the content length"
-              (should (string-contains? (str "Content-Length: " 
-                                             (:content-length response-map))
-                                        response-header))))
-
 (describe "#print-response"
-          (before-all (def tracker (atom []))
-                      (def client-reader (mock-client-reader full-header))
-                      (def client-writer (mock-client-writer tracker))
-                      (generate-request)
-                      (generate-response)
-                      (def response (str response-header "\r\n" response-body))
-                      (print-response client-reader client-writer)) 
-          (it "merges the response header and body"
-              (should= response (some #{response} @tracker))))
+  (let [output-tracker (track-request (sample-request-header "sample.txt"))
+        output (last @output-tracker)]
+    (it "includes the status code"
+      (should (string-contains? "HTTP/1.1 200 OK" output)))
+    (it "includes the host"
+      (should (string-contains? "Host: localhost:8080" output)))
+    (it "includes the content type"
+      (should (string-contains? "Content-Type: text/html" output)))
+    (it "includes the content length"
+      (should (string-contains? "Content-Length: 7" output))))
+
+  (let [output-tracker (track-request (sample-request-header "sample.txt"))]
+    (it "merges the response header and body"
+      (should= (sample-request-output) (last @output-tracker)))))
--- src/webserver/request_handler.clj
+++ src/webserver/request_handler.clj
@@ -1,64 +1,28 @@
 (ns webserver.request-handler
   (:require [webserver.request :refer [map-request]]
-            [webserver.response :refer [resource-response]]))
+            [webserver.response :refer [resource-response]]
+            [clojure.string :refer [join]]))
 
-(defn read-request-header [client-reader]
+(defn- newline [] "\r\n")
+
+(defn- read-request-header [client-reader]
   (loop [header "" line (.readLine client-reader)]
     (if (< 0 (.length line))
-      (recur (str header line "\r\n") (.readLine client-reader))
+      (recur (str header line (newline)) (.readLine client-reader))
       header)))
 
-(defn parse-get-request [string]
-  ((fn [string] (if (not (nil? string)) (last string)))
-     (re-find #"^GET (.*) HTTP" string)))
-
-(defn get-header-lines [client-reader]
-  (loop [prev nil line (.readLine client-reader) lines []] 
-    (if (< 0 (.length line)) 
-      (recur line (.readLine client-reader) (conj lines line))
-      lines)))
-
-(defn get-host [header-lines]
-  (loop [lines header-lines]
-    (if-not (empty? lines)
-      (let [host-value (re-find #"^Host: (.*)" (first lines))]
-        (if (nil? host-value)
-          (recur (rest lines))
-          (last host-value))))))
-
-(defn respond-to-request [client-reader client-writer]
-  (let [header-lines (get-header-lines client-reader)]
-    (let [[host port] (clojure.string/split (get-host header-lines) #":")
-          path (parse-get-request (first header-lines))]
-      (.println client-writer host)
-      (.println client-writer port)
-      (.println client-writer path))))
-
-(defn map-request-fields [header-lines]
-  (map-request (clojure.string/join "\r\n"  header-lines)))
-
-(defn get-response-body [request-map]
-  (let [[host-name host-port] (clojure.string/split (:host request-map) #":")]
-    (str "<p>" host-name "</p>"
-         "<p>" host-port "</p>"
-         "<p>" (:path request-map) "</p>")))
-
-(defn get-response-map [header-lines response-body]
-  {:status 200,
-   :host (get-host header-lines),
-   :content-type "text/html",
-   :content-length (.length response-body)})
-
-(defn get-response-header [response-map]
-  (str "HTTP/1.1 200 OK\r\n"
-       "Host: " (:host response-map) "\r\n"
-       "Content-Type: " (:content-type response-map) "\r\n"
-       "Content-Length: " (:content-length response-map) "\r\n"))
+(defn- flatten-header [response]
+  (let [header-map (:header response)]
+    (join (newline) 
+        [(:status-message header-map)
+         (str "Host: " (:host header-map))
+         (str "Content-Type: " (:content-type header-map))
+         (str "Content-Length: " (:content-length header-map) (newline))])))
 
 (defn print-response [client-reader client-writer]
-  (let [header-lines (get-header-lines client-reader)
-        request-fields (map-request-fields header-lines)
-        response-body (get-response-body request-fields)
-        response-map (get-response-map header-lines response-body)
-        response-header (get-response-header response-map)]
-    (.println client-writer (str response-header "\r\n" response-body))))
+  (let [request (map-request (read-request-header client-reader))
+        response (resource-response request)]
+    (.println client-writer
+              (str (flatten-header response)
+                   (newline)
+                   (:body response)))))
