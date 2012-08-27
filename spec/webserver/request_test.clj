62a97a72d6202865efc90d09588a588a68372bff
Jeremy Neander <subjectego@gmail.com>
2012-08-26 21:32:54 -0500

    Make #map-request parse the host

--- spec/webserver/request_test.clj
+++ spec/webserver/request_test.clj
@@ -2,16 +2,26 @@
   (:require [webserver.request :refer :all]
             [speclj.core :refer [describe it should=]]))
 
+(defn- request-header []
+  (str "GET /foo/bar HTTP/1.1\n"
+       "Host: localhost:8080\n"))
+
 (describe "#map-request"
+
   (it "maps a root-level GET request"
     (let [raw-request "GET / HTTP/1.1"
           request (map-request raw-request)]
       (should= "GET" (:type request))
       (should= "/" (:path request))
       (should= "1.1" (:http request))))
+
   (it "maps a nested directory request"
     (let [raw-request "PUT /stuff/more/stop/ HTTP/1.0"
           request (map-request raw-request)]
       (should= "PUT" (:type request))
       (should= "/stuff/more/stop/" (:path request))
-      (should= "1.0" (:http request)))))
+      (should= "1.0" (:http request))))
+
+  (it "maps the host"
+    (let [request (map-request (request-header))]
+      (should= "localhost:8080" (:host request)))))
--- src/webserver/request.clj
+++ src/webserver/request.clj
@@ -9,7 +9,11 @@
 (defn- request-http [raw-request]
   (last (re-find #"HTTP/(.*)" raw-request)))
 
+(defn- request-host [raw-request]
+  (last (re-find #"Host: (.*)" raw-request)))
+
 (defn map-request [raw-request]
   {:type (request-type raw-request)
    :path (request-path raw-request)
-   :http (request-http raw-request)})
+   :http (request-http raw-request)
+   :host (request-host raw-request)})
