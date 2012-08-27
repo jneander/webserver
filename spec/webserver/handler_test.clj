204b1ab6ffaeb10ee5d964d069c7decc329efe68
Jeremy Neander <subjectego@gmail.com>
2012-08-27 15:20:18 -0500

    Copy library /request_handler to /handler

--- spec/webserver/handler_test.clj
+++ spec/webserver/handler_test.clj
@@ -1,11 +1,61 @@
 (ns webserver.handler-test
   (:require [webserver.handler :refer :all]
-            [speclj.core :refer [describe it should=]]))
+            [speclj.core :refer [describe it should= should]])
+  (:import [java.io BufferedReader StringReader PrintStream OutputStream]))
 
 (defn- request [path]
   {:type "GET" :path (str "/spec/public_html" path) :http "1.1"})
+
+(defn- sample-request-header [path]
+  (str "GET /spec/public_html/" path " HTTP/1.1\r\n"
+       "Host: localhost:8080\r\n\r\n"))
+
+(defn- sample-request-output []
+  (str "HTTP/1.1 200 OK\r\n"
+       "Host: localhost:8080\r\n"
+       "Content-Type: text/html\r\n"
+       "Content-Length: 7\r\n"
+       "\r\n"
+       "foobar\n"))
+
+(defn- mock-client-reader [string]
+  (BufferedReader. (StringReader. string)))
+
+(defn- mock-client-writer [tracker]
+  (let [output-stream (proxy [OutputStream] [])]
+    (proxy [PrintStream] [output-stream]
+      (println [arg] (swap! tracker conj arg)))))
+
+(defn- tracker []
+  (atom []))
+
+(defn- track-request [request]
+  (let [output-tracker (tracker)
+        client-reader (mock-client-reader request)
+        client-writer (mock-client-writer output-tracker)]
+    (print-response client-reader client-writer)
+    output-tracker))
+
+(defn- string-contains? [pattern source]
+  (= pattern (re-find (re-pattern pattern) source)))
 
 (describe "#route-request"
   (it "sends directory requests to directory-response"
     (let [response (route-request (request "/sample_directory"))]
       (should= "<p>file3.txt</p>" (:body response)))))
+
+(describe "#print-response"
+  (let [output-tracker (track-request (sample-request-header "sample.txt"))
+        output (last @output-tracker)]
+    (it "includes the status code"
+      (should (string-contains? "HTTP/1.1 200 OK" output)))
+    (it "includes the host"
+      (should (string-contains? "Host: localhost:8080" output)))
+    (it "includes the content type"
+      (should (string-contains? "Content-Type: text/html" output)))
+    (it "includes the content length"
+      (should (string-contains? "Content-Length: 7" output))))
+
+  (let [output-tracker (track-request (sample-request-header "sample.txt"))]
+    (it "merges the response header and body"
+      (should= (sample-request-output) (last @output-tracker)))))
--- src/webserver/handler.clj
+++ src/webserver/handler.clj
@@ -1,6 +1,10 @@
 (ns webserver.handler
-  (:require [webserver.response :refer [resource-response]])
+  (:require [webserver.request :refer [map-request]]
+            [webserver.response :refer [resource-response]]
+            [clojure.string :refer [join]])
   (:import [java.io File]))
+
+(defn- line-ending [] "\r\n")
 
 (defn- server-directory []
   (.getCanonicalPath (File. ".")))
@@ -9,3 +13,25 @@
 
 (defmethod route-request :default [request]
   (resource-response request))
+
+(defn- read-request-header [client-reader]
+  (loop [header "" line (.readLine client-reader)]
+    (if (< 0 (.length line))
+      (recur (str header line (line-ending)) (.readLine client-reader))
+      header)))
+
+(defn- flatten-header [response]
+  (let [header-map (:header response)]
+    (join (line-ending) 
+        [(:status-message header-map)
+         (str "Host: " (:host header-map))
+         (str "Content-Type: " (:content-type header-map))
+         (str "Content-Length: " (:content-length header-map) (line-ending))])))
+
+(defn print-response [client-reader client-writer]
+  (let [request (map-request (read-request-header client-reader))
+        response (resource-response request)]
+    (.println client-writer
+              (str (flatten-header response)
+                   (line-ending)
+                   (:body response)))))
