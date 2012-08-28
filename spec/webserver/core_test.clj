0c1decbea0c2c9f782d729f2f5ff1edd1ee672cb
Jeremy Neander <subjectego@gmail.com>
2012-08-27 22:45:46 -0500

    Add method #parse-arguments
    This method should receive a vector of string arguments from the #main
    method. They should be parsed for flags and values according to the
    following rules:
     - '-p' for the port number, must be Number
     - '-d' for the directory

--- spec/webserver/core_test.clj
+++ spec/webserver/core_test.clj
@@ -1,2 +1,17 @@
 (ns webserver.core-test
-  (:use speclj.core))
+  (:require [speclj.core :refer [describe it should=]]
+            [webserver.core :refer [parse-arguments]]))
+
+(describe "#parse-arguments"
+
+  (it "parses acceptable values"
+    (let [args ["-p" "7000" "-d" "/foo/bar"]
+          parsed (parse-arguments args)]
+      (should= 7000 (:port parsed))
+      (should= "/foo/bar" (:directory parsed))))
+
+  (it "uses default values"
+    (let [args ["p" "7000" "d" "/foo/bar"]
+          parsed (parse-arguments args)]
+      (should= 8080 (:port parsed))
+      (should= "." (:directory parsed)))))
--- src/webserver/core.clj
+++ src/webserver/core.clj
@@ -3,8 +3,14 @@
              :refer [open-server-socket listen-and-respond]]
             [webserver.handler :refer [print-response]]))
 
+(defn parse-arguments [args]
+  (let [mapped (apply hash-map args)]
+    {:port (if (contains? mapped "-p") (read-string (get mapped "-p")) 8080)
+     :directory (if (contains? mapped "-d") (get mapped "-d") ".")}))
+
 (defn -main [& args]
-  (let [port (if (empty? args) 8080 (read-string (first args)))
+  (let [arg-map (parse-arguments args)
+        port (if (empty? args) 8080 (read-string (first args)))
         server-socket (open-server-socket port)]
     (while (not (.isClosed server-socket))
       (listen-and-respond server-socket print-response))))
